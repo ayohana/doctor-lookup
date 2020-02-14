a666a92021f9cf304e28e546f8849d53fc057d97
Adela Darmansyah <adela.yohana@gmail.com>
2020-02-14 14:13:59 -0800

    Remove Handlebars plugin and Fix query URL address request

---
+++ src/doctor-service.js
@@ -0,0 +1,11 @@
+export class DoctorService {
+  async getList(symptom){
+    try {
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?${symptom}&location=wa-seattle&sort=distance-asc&skip=0&limit=5&user_key=${process.env.API_KEY}`);
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+    } catch(error) {
+      console.error("There was an error handling your request: " + error.message);
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,58 +2,26 @@ import 'bootstrap' ;
 import 'bootstrap/dist/css/bootstrap.min.css' ; 
 import '../src/styles.css' ;
 import $ from 'jquery';
-// import { DoctorService } from '../src/doctor-service';
-const Handlebars = require("handlebars");
+import { DoctorService } from '../src/doctor-service';
 
 $(document).ready(function(){
 
+  $("form#patientInfo").submit(function(event){
+    event.preventDefault();
+    let symptom = $("#symptom").val();
+    $("#output").text(symptom);
 
-  // const template = Handlebars.compile("Name: {{name}}");
-  // console.log(template({ name: "Nils" }));
+    (async () => {
+      let doctorService = new DoctorService();
+      const response = await doctorService.getList(symptom);
+      displayList(response);
+    })();
 
-  // var source = document.getElementById("entry-template").innerHTML;
-  // var template = Handlebars.compile(source);
-
-  // var context = { title: "My New Post", body: "This is my first post!" };
-  // var html = template(context);
-  // console.log(html);
-
-  var api_key = process.env.API_KEY;
-
-  var resource_url = 'https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=pediatrician&location=wa-seattle&skip=1&limit=5&user_key=' + api_key;
-
-  $.get(resource_url, function (data, status) {
-    // data: { meta: {<metadata>}, data: {<array[Practice]>} }
-    var template = Handlebars.compile(document.getElementById('docs-template').innerHTML);
-    document.getElementById('content-placeholder').innerHTML = template(data);
-    console.log(template(data));
-    console.log(status);
+    const displayList = function(response) {
+      console.log(response);
+      console.log(response.data[0].profile.first_name);
+      console.log(response.data[0].specialties[0].name);
+      $("#output").text(symptom);
+    }
   });
-
-  // var conditions_resource_url = 'https://api.betterdoctor.com/2016-03-01/conditions?user_key=' + api_key;
-  // $.get(conditions_resource_url, function (data) {
-  //   // data: { meta: {<metadata>}, data: {<array[Condition]>} }
-    
-  //   var template = Handlebars.compile(document.getElementById('conditions-template').innerHTML);
-  //   document.getElementById('conditions-content-placeholder').innerHTML = template(data);
-  // });
-
-
-
-  // $("form#patientInfo").submit(function(event){
-  //   event.preventDefault();
-  //   let symptom = $("#symptom").val();
-  //   $("#output").text(symptom);
-
-  //   (async () => {
-  //     let doctorService = new DoctorService();
-  //     const response = await doctorService.getList(symptom);
-  //     displayList(response);
-  //   })();
-
-  //   const displayList = function(response) {
-  //     console.log(response.meta);
-  //     $("#output").text(symptom + response.data);
-  //   }
-  // });
 });
