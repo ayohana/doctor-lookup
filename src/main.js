54566fa2727e3cc47eebf40f880c4c60568b67ed
Adela Darmansyah <adela.yohana@gmail.com>
2020-02-14 09:58:07 -0800

    Create a blank form and Gather input for medical symptom

--- src/main.js
+++ src/main.js
@@ -2,3 +2,11 @@ import 'bootstrap' ;
 import 'bootstrap/dist/css/bootstrap.min.css' ; 
 import './styles.css' ;
 import $ from 'jquery';
+
+$(document).ready(function(){
+  $("form#patientInfo").submit(function(event){
+    event.preventDefault();
+    let inputSymptom = $("#symptom").val();
+    $("#output").text(inputSymptom);
+  });
+});
