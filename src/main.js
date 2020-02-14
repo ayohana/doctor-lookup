a1d8bed6eaf82c19e2e571312766a9b4d9b75510
Adela Darmansyah <adela.yohana@gmail.com>
2020-02-14 10:57:54 -0800

    Install Handlebars plugin and Test its implementation

--- src/main.js
+++ src/main.js
@@ -1,12 +1,33 @@
 import 'bootstrap' ; 
 import 'bootstrap/dist/css/bootstrap.min.css' ; 
-import './styles.css' ;
+import '../src/styles.css' ;
 import $ from 'jquery';
+// import { DoctorService } from '../src/doctor-service';
+const Handlebars = require("handlebars");
 
 $(document).ready(function(){
-  $("form#patientInfo").submit(function(event){
-    event.preventDefault();
-    let inputSymptom = $("#symptom").val();
-    $("#output").text(inputSymptom);
-  });
+
+
+  const template = Handlebars.compile("Name: {{name}}");
+  console.log(template({ name: "Nils" }));
+
+
+
+
+  // $("form#patientInfo").submit(function(event){
+  //   event.preventDefault();
+  //   let symptom = $("#symptom").val();
+  //   $("#output").text(symptom);
+
+  //   (async () => {
+  //     let doctorService = new DoctorService();
+  //     const response = await doctorService.getList(symptom);
+  //     displayList(response);
+  //   })();
+
+  //   const displayList = function(response) {
+  //     console.log(response.meta);
+  //     $("#output").text(symptom + response.data);
+  //   }
+  // });
 });
