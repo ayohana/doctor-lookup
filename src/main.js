3bcfc5129dcb02af143f9ffe4f77165d31996656
Adela Darmansyah <adela.yohana@gmail.com>
2020-02-14 11:21:29 -0800

    Test sample code using Handlebars for a list of conditions and doctors

--- src/main.js
+++ src/main.js
@@ -8,9 +8,33 @@ const Handlebars = require("handlebars");
 $(document).ready(function(){
 
 
-  const template = Handlebars.compile("Name: {{name}}");
-  console.log(template({ name: "Nils" }));
+  // const template = Handlebars.compile("Name: {{name}}");
+  // console.log(template({ name: "Nils" }));
 
+  // var source = document.getElementById("entry-template").innerHTML;
+  // var template = Handlebars.compile(source);
+
+  // var context = { title: "My New Post", body: "This is my first post!" };
+  // var html = template(context);
+  // console.log(html);
+
+  var api_key = process.env.API_KEY; // Get your API key at developer.betterdoctor.com
+
+  var resource_url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=wa-seattle&skip=2&limit=10&user_key=' + api_key;
+
+  $.get(resource_url, function (data) {
+    // data: { meta: {<metadata>}, data: {<array[Practice]>} }
+    var template = Handlebars.compile(document.getElementById('docs-template').innerHTML);
+    document.getElementById('content-placeholder').innerHTML = template(data);
+  });
+
+  var conditions_resource_url = 'https://api.betterdoctor.com/2016-03-01/conditions?user_key=' + api_key;
+  $.get(conditions_resource_url, function (data) {
+      // data: { meta: {<metadata>}, data: {<array[Condition]>} }
+      
+      var template = Handlebars.compile(document.getElementById('conditions-template').innerHTML);
+      document.getElementById('conditions-content-placeholder').innerHTML = template(data);
+  });
 
 
 
