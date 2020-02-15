894194a28dbd95688c0a095fe1ea962fb63eecc1
Adela Darmansyah <adela.yohana@gmail.com>
2020-02-14 18:02:03 -0800

    Update URL to display results in descending order starting from best-rated doctor

--- src/doctor-service.js
+++ src/doctor-service.js
@@ -1,7 +1,7 @@
 export class DoctorService {
   async getList(symptom, lastName){
     try {
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${lastName},${symptom}&location=wa-seattle&sort=best-match-asc&skip=0&limit=5&user_key=${process.env.API_KEY}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${lastName},${symptom}&location=wa-seattle&sort=best-match-desc&skip=0&limit=5&user_key=${process.env.API_KEY}`;
       let response = await fetch(url);
       console.log(url);
       let jsonifiedResponse = await response.json();
